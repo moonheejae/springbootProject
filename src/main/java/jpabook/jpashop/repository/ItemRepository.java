c94ad42bc8348054041eb784728e4b29876e701b
heejae <moon49051130@gmail.com>
2024-04-19 02:36:02 +0900

    Add Order feature

--- src/main/java/jpabook/jpashop/repository/ItemRepository.java
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -3,18 +3,21 @@ package jpabook.jpashop.repository;
 import jakarta.persistence.EntityManager;
 import jpabook.jpashop.domain.item.Item;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 @RequiredArgsConstructor
+@Slf4j
 public class ItemRepository {
 
     private final EntityManager em;
 
     public void save(Item item){
         if( item.getId() == null ) {
+            log.info("item={}", item);
             em.persist(item);
         } else {
             em.merge(item);
